7830d06dafa489b68a3b894d8914aba6bf14aa7c
Kenny Lin <r06723025@ntu.edu.tw>
2019-03-19 20:54:16 +0800

    update sketch.js and add readme.md

--- Homework02/sketch.js
+++ Homework02/sketch.js
@@ -1,11 +1,11 @@
 let cvsWrapper = null;
 let x1, y1, mid_x, mid_y, vy, ay, rotAngle;
-let scrollSpeed = 2;
+let scrollSpeed;
 let assets = {}
 let birdFlap, birdIdx;
 let ground;
 let status, point;
-let bird, game;
+let bird, game, pipes, timestamp;
 // assets from: https://github.com/sourabhv/FlapPyBird/tree/master/assets
 
 function flappyBird(){
@@ -28,9 +28,22 @@ function flappyBird(){
 }
 
 function pipe (){
-    this.x = width - game.x1;
-    this.y = height;
-    this.gapDiff = random(50, height-50);
+    this.w = 52
+    this.h = 320
+    this.x = -game.x1+width;
+    this.yTop = 0;
+    this.yDown = height-assets["base"].height-100;
+    this.randTop = random(0, 200);
+    //this.randDown = random(100, 250);
+    this.checkHit = function (){
+        console.log(bird.x, this.x+game.x1, this.x+this.w+game.x1)
+        //console.log(width)
+        if (bird.y > height-assets["base"].height-90-this.randTop || bird.y < this.h-this.randTop-10){
+            if (bird.x > this.x+game.x1-5 && bird.x < this.x+this.w+game.x1+5){
+                game.status = "hitTop";
+            }
+        }
+    }
 }
 
 function newGame(){
@@ -44,6 +57,9 @@ function newGame(){
     this.birdIdx = Math.floor(Math.random()*3);
     this.bgIdx = Math.floor(Math.random()*2);
     this.hitFlag = false;
+    this.timestamp = 0;
+    this.pipeRate = 80;
+    this.scrollSpeed = 1;
 
     this.restart = function(){
         this.status = "start"; //start, play, gameover
@@ -56,6 +72,9 @@ function newGame(){
         this.birdIdx = Math.floor(Math.random()*3)
         this.bgIdx = Math.floor(Math.random()*2);
         this.hitFlag = false;
+        this.timestamp = 0;
+        this.pipeRate = 80;
+        this.scrollSpeed = 1;
         }
 }
 
@@ -90,6 +109,8 @@ function setup() {
     game = new newGame();
     bird = new flappyBird();
     birdFlap = 0;
+    pipes = [];
+    pipes.push(new pipe());
 }
 
 function draw() {
@@ -98,10 +119,40 @@ function draw() {
     translate(game.x1, game.y1);
     image(assets["bg"][game.bgIdx], 0, 0, width, height);
     image(assets["bg"][game.bgIdx], width, 0, width, height);
-    image(assets["pipe"][0][1], width, 0)
-    image(assets["pipe"][0][0], width, height-assets["base"].height-100);
+    //console.log(pipes.length)
+    /*
+    if (game.timestamp % 300 === 0){
+        if (game.pipeRate > 20) game.pipeRate -= 10;
+    }
+    */
+    if (game.timestamp % game.pipeRate === 0 && game.status === "play"){
+        pipes.push(new pipe());
+        //console.log(-game.x1+width)
+    }
+    //console.log(assets["pipe"][0][0].width, assets["pipe"][0][1].height)
+    if (game.status !== "start"){
+        for (let i=0; i < pipes.length; i++){
+            if (game.status === "play") pipes[i].x -= game.scrollSpeed;
+            //console.log(pipes[i].x);
+            if (pipes[i].x > -game.x1-50){
+                
+                image(assets["pipe"][0][1], pipes[i].x, -pipes[i].randTop)
+                image(assets["pipe"][0][0], pipes[i].x, height-assets["base"].height-100-pipes[i].randTop);
+            }
+            else{
+                pipes.splice(i,1)
+            }
+            if (game.status === "play") pipes[i].checkHit();
+            //if (pipes[i].x < )
+            //pipes.splice(i,1);
+        }
+    }
+    //console.log(bird.y); 
+    //image(assets["pipe"][0][1], width, 0)
+    //image(assets["pipe"][0][0], width, height-assets["base"].height-100);
     image(assets["base"], 0, height-assets["base"].height, width);
     image(assets["base"], width, height-assets["base"].height, width);
+
     //console.log(status);
     translate(bird.x-game.x1, bird.y);
     if (game.status === "start"){
@@ -121,17 +172,24 @@ function draw() {
     if (birdFlap != 17) birdFlap += 1;
     else birdFlap = 0;
     if (game.status === "start" || game.status === "play"){
-        game.x1 -= scrollSpeed;
-        if (game.x1 < -width) game.x1 = 0;
+        game.x1 -= game.scrollSpeed;
+        if (game.x1 < -width){
+            game.x1 = 0;
+            for (let i=0; i < pipes.length; i++){
+                pipes[i].x -= width;
+            }
+        }
     }
     bird.checkPosition();
-    //console.log(game.status)
+    game.timestamp += 1;
+    //translate(game.x1, game.y1);
+
 }
 
 function keyPressed() {
     if (keyCode == 32){
         if (game.status === "play"){
-            game.vy = -8;
+            game.vy = -6;
             assets["audio"][4].play();
             game.rotAngle = -PI/6;
         }
@@ -142,6 +200,7 @@ function keyPressed() {
             game.restart();
             bird.x = width/2;
             bird.y = height/2;
+            pipes = [];
         }
     }
 }
